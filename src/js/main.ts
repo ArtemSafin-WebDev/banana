e0f4d9ea8d84e41838e8ca7450f183b18d699657
Artem Safin <artemsafin.webdev@gmail.com>
2024-03-28 11:38:17 +0300

    Блоки

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,12 +4,18 @@ import accordions from "./accordions";
 import menu from "./menu";
 import modals from "./modals";
 import forms from "./forms";
+import ecosystem from "./ecosystem";
+import funding from "./funding";
+import blog from "./blog";
 
 document.addEventListener("DOMContentLoaded", () => {
   accordions();
   menu();
   modals();
   forms();
+  ecosystem();
+  funding();
+  blog();
 });
 
 window.addEventListener("load", () => {
